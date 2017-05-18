860eb9ca2dcf79db55e1480e8c3c5a478e622c29
TheScarastic <warabhishek@gmail.com>
2017-05-18 08:08:11 +0000

    fs: pstore: Import pstore Changes

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -61,12 +61,12 @@ static ulong ramoops_pmsg_size = MIN_MEM_SIZE;
 module_param_named(pmsg_size, ramoops_pmsg_size, ulong, 0400);
 MODULE_PARM_DESC(pmsg_size, "size of user space message log");
 
-static ulong mem_address;
+static ulong mem_address = 0x9ff00000;
 module_param(mem_address, ulong, 0400);
 MODULE_PARM_DESC(mem_address,
 		"start of reserved RAM used to store oops/panic logs");
 
-static ulong mem_size;
+static ulong mem_size = 0x100000;
 module_param(mem_size, ulong, 0400);
 MODULE_PARM_DESC(mem_size,
 		"size of reserved RAM used to store oops/panic logs");
